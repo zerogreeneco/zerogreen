ce27d9ca94224044082597e84c2cef8ac096ba33
iamgood147@gmail.com <iamgood147@gmail.com>
2022-01-17 01:26:27 +0900

    01.17 01:25 favicon추가, 가게상세페이지 레이아웃잡기

---
+++ src/main/java/zerogreen/eco/controller/StoreController.java
@@ -0,0 +1,20 @@
+package zerogreen.eco.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Log4j2
+@RequiredArgsConstructor
+@RequestMapping("/store")
+public class StoreController {
+
+    @GetMapping("/detail")
+    public void list(){
+
+    }
+
+}
