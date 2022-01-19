d460b9a44facfb4461abd90d7d995fe13e3151e6
DDDZZZ <dylee0568@gmail.com>
2022-01-20 00:27:34 +0900

    회원가입 이메일 인증 -> CSS 수정 중

--- src/main/java/zerogreen/eco/service/mail/MailService.java
+++ src/main/java/zerogreen/eco/service/mail/MailService.java
@@ -1,6 +1,7 @@
 package zerogreen.eco.service.mail;
 
 public interface MailService {
-    public void sendAuthMail(String mail, String key);
+    void sendAuthMail(String mail, String key);
 
+    String createAuthKey();
 }
