fd3e6f8708148ff987f34e16edf1f2aabec8e8a2
DDDZZZ <dylee0568@gmail.com>
2022-01-24 23:46:25 +0900

    가게 회원 회원가입시 주소 전체 받기 Ok 사업자 등록증 미첨부 관련 에러 해결 및 에러 메시지 출력 ok errors.properties 에러 메시지 수정

--- src/main/java/zerogreen/eco/TestDataInit.java
+++ src/main/java/zerogreen/eco/TestDataInit.java
@@ -5,14 +5,12 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import zerogreen.eco.entity.file.RegisterFile;
 import zerogreen.eco.entity.userentity.*;
-import zerogreen.eco.repository.user.StoreMemberRepository;
 import zerogreen.eco.service.user.BasicUserService;
 import zerogreen.eco.service.user.MemberService;
 import zerogreen.eco.service.user.StoreMemberService;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
-import java.time.LocalDateTime;
 
 @Component
 @RequiredArgsConstructor
@@ -35,27 +33,27 @@ public class TestDataInit {
         private final StoreMemberService storeMemberService;
         private final BasicUserService basicUserService;
 
-        private final StoreMemberRepository storeMemberRepository;
-
         public void init() {
             StoreMember ecoTest = new StoreMember("ecoTest", "01033334444", "1",
-                    UserRole.UNSTORE, "ECO_SHOP", "REGNUM1111",StoreType.ECO_SHOP,
+                    UserRole.UN_STORE, "ECO_SHOP", "REGNUM1111",StoreType.ECO_SHOP,
                     "부산시 해운대구", "0517778888",  "11111");
+
             StoreMember foodTest = new StoreMember("foodTest", "01044445555", "1",
-                    UserRole.UNSTORE, "VEGAN_FOOD", "0987654321", StoreType.VEGAN_FOOD,
+                    UserRole.UN_STORE, "VEGAN_FOOD", "0987654321", StoreType.VEGAN_FOOD,
                     "부산시 동래구", "0519998888", "99999");
+
             StoreMember noVegan = new StoreMember("noVeganlTest", "01044445555", "1",
-                    UserRole.UNSTORE, "NO_VEGAN_FOOD", "785654321", StoreType.GENERAL_FOOD,
+                    UserRole.UN_STORE, "NO_VEGAN_FOOD", "785654321", StoreType.GENERAL_FOOD,
                     "부산시 연제구", "0517776666", "88888");
 
-            ecoTest.setStoreInfo(new StoreInfo("부산시 해운대구", "0519998888",null,
-                    LocalDateTime.now(), LocalDateTime.now()));
-
+            // 일반 회원
             memberService.save(new Member("test", "tester", "01022223333", "1",
                     UserRole.USER, VegetarianGrade.LACTO));
+            // 가게 회원
             storeMemberService.save(ecoTest, new RegisterFile("origin1","store1", "path1"));
             storeMemberService.save(foodTest, new RegisterFile("origin2","store2", "path2"));
             storeMemberService.save(noVegan, new RegisterFile("origin3","store3", "path3"));
+            // 관리자 계정
             basicUserService.adminSave();
 
             em.flush();
--- src/main/java/zerogreen/eco/entity/userentity/VegetarianGrade.java
+++ src/main/java/zerogreen/eco/entity/userentity/VegetarianGrade.java
@@ -1,8 +1,8 @@
 package zerogreen.eco.entity.userentity;
 
 public enum VegetarianGrade {
-    VEGAN("비건"), LACTO("락토"), OVO("오보"),
-    LACTO_OVO("락토-오보"), PESCO("페스코"), POLLO("폴로"), PLEXITARIAN("플렉시타리안");
+    VEGAN("비건"), LACTO("락토 베지테리언"), OVO("오보 베지테리언"),
+    LACTO_OVO("락토-오보 베지테리언"), PESCO("페스코 베지테리언"), POLLO("폴로 베지테리언"), PLEXITARIAN("플렉시타리언");
 
     private final String vegetarian;
 
