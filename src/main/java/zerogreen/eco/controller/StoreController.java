c43801cea00879cba70e245eea1c05ffa19ae85c
Kimgeunhye <geunhyengineer@naver.com>
2022-01-24 23:51:17 +0900

    -가게 리스트 페이지 구성 작업 중

---
+++ src/main/java/zerogreen/eco/controller/StoreController.java
@@ -0,0 +1,20 @@
+package zerogreen.eco.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping
+public class StoreController {
+
+    @GetMapping("/page/list")
+    public void list(){
+
+
+    }
+}
