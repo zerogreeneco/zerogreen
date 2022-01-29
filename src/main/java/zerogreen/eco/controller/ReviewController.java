fd587e0f42235cb73a335782f60c4998cddb3561
iamgood147@gmail.com <iamgood147@gmail.com>
2022-01-29 22:42:55 +0900

    01.29 리뷰 수정중

--- src/main/java/zerogreen/eco/controller/ReviewController.java
+++ src/main/java/zerogreen/eco/controller/ReviewController.java
@@ -8,8 +8,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import zerogreen.eco.dto.detail.MemberReviewDto;
+import zerogreen.eco.dto.store.StoreDto;
+import zerogreen.eco.entity.detail.MemberReview;
+import zerogreen.eco.entity.userentity.BasicUser;
+import zerogreen.eco.entity.userentity.StoreMember;
+import zerogreen.eco.security.auth.PrincipalUser;
 import zerogreen.eco.service.detail.ReviewService;
+import zerogreen.eco.service.user.StoreMemberService;
 
 @Controller
 @Slf4j
@@ -18,10 +26,39 @@ import zerogreen.eco.service.detail.ReviewService;
 public class ReviewController {
 
     private final ReviewService reviewService;
+    private final StoreMemberService storeMemberService;
 
-    @RequestMapping("/addReview/{id}")
-    public ResponseEntity<Long> addReview(@RequestBody MemberReviewDto memberReviewDto) {
-        Long rno = reviewService.saveReview(memberReviewDto);
-        return new ResponseEntity<>(rno, HttpStatus.OK);
+    @PostMapping("/addReview/{id}")
+    @ResponseBody
+    public ResponseEntity<Long> addReview(@RequestBody MemberReviewDto memberReviewDto,
+                                          @PrincipalUser BasicUser basicUser,
+                                          Long id) {
+        if (basicUser != null) {
+            MemberReview review = new MemberReviewDto().reviewDto(memberReviewDto);
+            log.info("--------1111111111" + review);
+            reviewService.saveReview(basicUser.getUsername(), review);
+            log.info("--------2222222222" + basicUser.getUsername(), review);
+        }
+            return new ResponseEntity<>(HttpStatus.OK);
     }
-}
+
+/*
+    public ResponseEntity<Long> addReview(@RequestBody MemberReviewDto memberReviewDto) {
+        MemberReview review = new MemberReviewDto().reviewDto(memberReviewDto);
+        reviewService.saveReview(review);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+*/
+
+/*
+    public String addReview(@RequestBody MemberReviewDto memberReviewDto,
+                            RedirectAttributes redirectAttributes) {
+        MemberReview review = new MemberReviewDto().reviewDto(memberReviewDto);
+        reviewService.saveReview(review);
+        return "redirect: page/detail";
+    }
+*/
+
+
+
+    }
--- src/main/java/zerogreen/eco/service/detail/ReviewServiceImpl.java
+++ src/main/java/zerogreen/eco/service/detail/ReviewServiceImpl.java
@@ -5,7 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import zerogreen.eco.dto.detail.MemberReviewDto;
 import zerogreen.eco.entity.detail.MemberReview;
+import zerogreen.eco.entity.userentity.BasicUser;
+import zerogreen.eco.entity.userentity.StoreMember;
 import zerogreen.eco.repository.detail.MemberReviewRepository;
+import zerogreen.eco.repository.user.BasicUserRepository;
+import zerogreen.eco.repository.user.StoreMemberRepository;
 
 @Service
 @Slf4j
@@ -13,12 +17,30 @@ import zerogreen.eco.repository.detail.MemberReviewRepository;
 public class ReviewServiceImpl implements ReviewService{
 
     private final MemberReviewRepository memberReviewRepository;
+    private final StoreMemberRepository storeMemberRepository;
+    private final BasicUserRepository basicUserRepository;
 
     //멤버리뷰 DB저장
     @Override
-    public Long saveReview(MemberReviewDto memberReviewDto) {
-        MemberReview memberReview = new MemberReview(memberReviewDto.getReviewText(),memberReviewDto.getStoreMember(),memberReviewDto.getBasicUser());
-        memberReviewRepository.save(memberReview);
-        return memberReview.getRno();
+    public Long saveReview(String username, MemberReview memberReview) {
+        log.info("--------44444444"+memberReview);
+        BasicUser findUser = basicUserRepository.findByUsername(username).orElseThrow();
+        log.info("-----------3333333"+findUser.getUsername());
+        return memberReviewRepository.save(new MemberReview(memberReview.getReviewText(),
+                        findUser))
+                .getRno();
     }
+
+        /*
+        return memberReviewRepository.save(new MemberReview(memberReview.getReviewText(),
+                        memberReview.getBasicUser().getUsername(),
+                        memberReview.getBasicUser().getId()))
+                .getRno();
+*/
+
+//        MemberReview memberReview = new MemberReview(memberReviewDto.getReviewText(),
+//                memberReviewDto.getStoreName(),memberReviewDto.getUsername());
+//        memberReviewRepository.save(memberReview);
+//        return memberReview.getRno();
+
 }
