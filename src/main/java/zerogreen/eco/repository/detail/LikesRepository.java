3ed8e15700cbf218878048e2183268ccf0f6537b
iamgood147@gmail.com <iamgood147@gmail.com>
2022-02-05 01:33:10 +0900

    02.05 라이크 추가, 삭제 가능. 비회원 좋아요 기능 처리. 비동기식처리 작업중

--- src/main/java/zerogreen/eco/repository/detail/LikesRepository.java
+++ src/main/java/zerogreen/eco/repository/detail/LikesRepository.java
@@ -3,6 +3,7 @@ package zerogreen.eco.repository.detail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestParam;
 import zerogreen.eco.dto.detail.LikesDto;
 import zerogreen.eco.entity.detail.Likes;
@@ -21,8 +22,9 @@ public interface LikesRepository extends JpaRepository<Likes, Long> {
 
     @Query("select count(l.id) from Likes l where l.storeMember.id =:sno " +
             "and l.basicUser.id =:mno")
-    int cntMemberLike(@RequestParam("sno") Long sno, @RequestParam("mno") Long mno);
+    Long cntMemberLike(@RequestParam("sno") Long sno, @RequestParam("mno") Long mno);
 
+    @Transactional
     @Modifying
     @Query("delete from Likes l where l.storeMember.id =:sno and l.basicUser.id =:mno ")
     void deleteMemberLikes(@RequestParam("sno") Long sno, @RequestParam("mno") Long mno);
--- src/main/java/zerogreen/eco/service/detail/LikesService.java
+++ src/main/java/zerogreen/eco/service/detail/LikesService.java
@@ -1,6 +1,5 @@
 package zerogreen.eco.service.detail;
 
-import zerogreen.eco.dto.detail.LikesDto;
 import zerogreen.eco.entity.userentity.BasicUser;
 
 public interface LikesService {
@@ -11,9 +10,6 @@ public interface LikesService {
     //나 얼만큼 좋아해요
     Long cntLikes(Long sno);
     //개별 라이크
-    int cntMemberLike(Long sno, Long mno);
-    //라이크 데이터
-    //LikesDto liking(Long id, String username);
-
+    Long cntMemberLike(Long sno, Long mno);
 
     }
