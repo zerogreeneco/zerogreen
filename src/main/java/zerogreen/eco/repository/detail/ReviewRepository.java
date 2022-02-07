f9f7dc9c455780a31a828c7fa84d3c1daa0fc8f9
DDDZZZ <dylee0568@gmail.com>
2022-02-07 11:50:24 +0900

    스토어 멤버 테스트 데이터 수정 멤버 리뷰 리포지토리 수정

--- src/main/java/zerogreen/eco/repository/detail/ReviewRepository.java
+++ src/main/java/zerogreen/eco/repository/detail/ReviewRepository.java
@@ -2,13 +2,11 @@ package zerogreen.eco.repository.detail;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import zerogreen.eco.dto.detail.MemberReviewDto;
 import zerogreen.eco.dto.detail.StoreReviewDto;
-import zerogreen.eco.entity.detail.MemberReview;
 import zerogreen.eco.entity.userentity.StoreMember;
 
-public interface ReviewRepository extends JpaRepository<MemberReview, Long>, ReviewRepositoryCustom {
+public interface ReviewRepository {
     Page<MemberReviewDto> findByStore(Pageable pageable, StoreMember storeMember);
 
     }
