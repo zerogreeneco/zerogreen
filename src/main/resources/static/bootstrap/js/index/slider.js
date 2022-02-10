454542e653ed2200585aa62610b08b742be326d6
aibeam <aibeam1025@gmail.com>
2022-02-10 16:35:34 +0900

    slider.js - 완성

--- src/main/resources/static/bootstrap/js/index/slider.js
+++ src/main/resources/static/bootstrap/js/index/slider.js
@@ -1,29 +1,33 @@
 $(document).ready(function() {
     $("#food-right").click(function() {
         $(".food-slider").animate({
-        marginLeft: "-=180px"
+        marginLeft: "-180px"
         }, function() {
-            $(".food-slider div:first").appendTo(".food-slider");
+            $(".food-slider .card:first").appendTo(".food-slider");
             $(".food-slider").css({
             marginLeft: 0
             });
         });
     });
     $("#food-left").click(function() {
-        $(".food-slider div:last").appendTo(".food-slider");
-        $(".food-slider").css({ "marginLeft", "-180px"});
+        $(".food-slider .card:last").prependTo(".food-slider");
+        $(".food-slider").css({ "marginLeft": "-180px"});
         $(".food-slider").animate({ marginLeft: 0 });
     });
 
     $("#shop-right").click(function() {
-        $(".shop-slider").animate({ marginLeft: "-=180px" }, function() {
-            $(".shop-slider div:first").appendTo(".shop-slider");
-            $(".shop-slider").css({ marginLeft: 0 });
+        $(".shop-slider").animate({
+        marginLeft: "-180px"
+        }, function() {
+            $(".shop-slider .card:first").appendTo(".shop-slider");
+            $(".shop-slider").css({
+            marginLeft: 0
+            });
         });
     });
     $("#shop-left").click(function() {
-        $(".shop-slider div:last").appendTo(".shop-slider");
-        $(".shop-slider").css({ "marginLeft", "-180px"});
+        $(".shop-slider .card:last").prependTo(".shop-slider");
+        $(".shop-slider").css({ "marginLeft": "-180px"});
         $(".shop-slider").animate({ marginLeft: 0 });
     });
 
