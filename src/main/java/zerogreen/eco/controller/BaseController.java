a2303105920503ecaef4cd49c2b48b5482a25a05
DDDZZZ <dylee0568@gmail.com>
2022-02-25 00:12:18 +0900

    커뮤니티 게시글 수정/삭제/채팅하기 버튼 -> 작성자만 수정/삭제 버튼 노출, 채팅하기 X -> 작성자 아닐 경우 채팅하기 버튼만 노출

--- src/main/java/zerogreen/eco/controller/BaseController.java
+++ src/main/java/zerogreen/eco/controller/BaseController.java
@@ -1,8 +1,12 @@
 package zerogreen.eco.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import zerogreen.eco.entity.userentity.VegetarianGrade;
+import zerogreen.eco.security.auth.PrincipalDetails;
+
+import javax.servlet.http.HttpSession;
 
 // 컨트롤러 전체에서 발생하는 예외를 한 곳에서 관리하고 처리하는 어노테이션
 // ModelAttribute를 사용할 경우, 컨트롤러 전체에서 사용할 수 있다.
@@ -13,4 +17,5 @@ public class BaseController {
         VegetarianGrade[] vegans = VegetarianGrade.values();
         return vegans;
     }
+
 }
