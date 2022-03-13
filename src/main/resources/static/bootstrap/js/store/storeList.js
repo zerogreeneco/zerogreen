568ed9f01b3762abe09ff2ea683e252daa08cb40
Kimgeunhye <geunhyengineer@naver.com>
2022-03-13 14:55:10 +0900

    무한 스크롤 작업

--- src/main/resources/static/bootstrap/js/store/storeList.js
+++ src/main/resources/static/bootstrap/js/store/storeList.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    let page = 1;
     if (window.location.href == "http://localhost:8080/zerogreen/food/list") {
         $("label:first").css('background', '#16a085');
         $("label:first").css('color', '#fff');
@@ -31,4 +32,47 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop: 0}, '400');
     });
 
+    // 더보기
+    $("#nextShop").click(function () {
+        ++page;
+        // let keyword = getParameterByName("keyword");
+        // let category = getParameterByName("category");
+        // let searchType = getParameterByName("searchType");
+
+        $.ajax({
+            url: "/zerogreen/shop/list",
+            method: "post",
+            data: {
+                page: page
+                // category: category,
+                // keyword: keyword,
+                // searchType: searchType
+            }
+        })
+            .done(function (fragment) {
+                $("#shop-align").append(fragment);
+            });
+    });
+
+    // 더보기
+    $("#nextFood").click(function () {
+        ++page;
+        // let keyword = getParameterByName("keyword");
+        // let category = getParameterByName("category");
+        // let searchType = getParameterByName("searchType");
+
+        $.ajax({
+            url: "/zerogreen/food/list",
+            method: "post",
+            data: {
+                page: page
+                // category: category,
+                // keyword: keyword,
+                // searchType: searchType
+            }
+        })
+            .done(function (fragment) {
+                $("#food-align").append(fragment);
+            });
+    });
 });
