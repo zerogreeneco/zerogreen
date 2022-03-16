9b823e143a249c0b1ee39c6fa4c6921c37ded1cd
Kimgeunhye <geunhyengineer@naver.com>
2022-03-16 23:46:46 +0900

    storeName 검색

--- src/main/java/zerogreen/eco/repository/list/StoreListRepositoryImpl.java
+++ src/main/java/zerogreen/eco/repository/list/StoreListRepositoryImpl.java
@@ -2,11 +2,16 @@ package zerogreen.eco.repository.list;
 
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.util.StringUtils;
+import zerogreen.eco.dto.search.SearchCondition;
+import zerogreen.eco.dto.search.StoreSearchType;
 import zerogreen.eco.dto.store.StoreDto;
 import zerogreen.eco.entity.file.QStoreImageFile;
 import zerogreen.eco.entity.userentity.StoreMember;
@@ -15,57 +20,48 @@ import zerogreen.eco.entity.userentity.StoreType;
 import javax.persistence.EntityManager;
 import java.util.List;
 
+import static zerogreen.eco.entity.detail.QLikes.likes;
 import static zerogreen.eco.entity.userentity.QStoreMember.storeMember;
 import static zerogreen.eco.entity.userentity.QStoreMenu.storeMenu;
 import static zerogreen.eco.entity.userentity.UserRole.STORE;
-import static zerogreen.eco.entity.detail.QLikes.likes;
 
 public class StoreListRepositoryImpl implements StoreListRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
-    public StoreListRepositoryImpl(EntityManager manager){this.jpaQueryFactory = new JPAQueryFactory(manager);}
+
+    public StoreListRepositoryImpl(EntityManager manager) {
+        this.jpaQueryFactory = new JPAQueryFactory(manager);
+    }
 
     QStoreImageFile storeImage = new QStoreImageFile("storeImage");
 
     @Override
     public Slice<StoreDto> getShopList(Pageable pageable) {
-        List<StoreDto> shopList = jpaQueryFactory
-                .select(Projections.constructor(StoreDto.class,
-                        storeMember.id,
-                        storeMember.storeName,
-                        storeMember.storeInfo.storePhoneNumber,
-                        storeMember.storeInfo.openTime,
-                        storeMember.storeInfo.closeTime,
-                        ExpressionUtils.as(
-                                JPAExpressions
-                                        .select(storeImage.thumbnailName)
-                                        .from(storeImage, storeImage)
-                                        .where(storeImage.id.eq(
-                                                JPAExpressions
-                                                        .select(storeImage.id.min())
-                                                        .from(storeImage, storeImage)
-                                                        .where(storeImage.storeMember.id.eq(storeMember.id)))),"listThumbnail"),
-                        ExpressionUtils.as(
-                                JPAExpressions
-                                        .select(storeMenu.menuName)
-                                        .from(storeMenu, storeMenu)
-                                        .where(storeMenu.id.eq(
-                                                JPAExpressions
-                                                        .select(storeMenu.id.min())
-                                                        .from(storeMenu, storeMenu)
-                                                        .where(storeMenu.storeMember.id.eq(storeMember.id)))),"menuName"),
-                        ExpressionUtils.as(
-                                JPAExpressions
-                                        .select(likes.id.count())
-                                        .from(likes, likes)
-                                        .where(likes.storeMember.id.eq(storeMember.id)),"likes")
-                        ))
-                .from(storeMember, storeMember)
-                .where(storeMember._super.userRole.eq(STORE),
-                        storeMember.storeType.eq(StoreType.ECO_SHOP))
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
+        List<StoreDto> shopList =
+                shopProjections()
+                        .where(storeMember._super.userRole.eq(STORE),
+                                storeMember.storeType.eq(StoreType.ECO_SHOP))
+                        .offset(pageable.getOffset())
+                        .limit(pageable.getPageSize())
+                        .fetch();
+
+        List<StoreMember> countQuery = jpaQueryFactory
+                .selectFrom(storeMember)
                 .fetch();
+
+        return new PageImpl<>(shopList, pageable, countQuery.size());
+    }
+
+    @Override
+    public Slice<StoreDto> getShopList(Pageable pageable, SearchCondition searchCondition) {
+        List<StoreDto> shopList =
+                shopProjections()
+                        .where(storeMember._super.userRole.eq(STORE),
+                                storeMember.storeType.eq(StoreType.ECO_SHOP),
+                                isSearch(searchCondition.getStoreSearchType(), searchCondition.getContent()))
+                        .offset(pageable.getOffset())
+                        .limit(pageable.getPageSize())
+                        .fetch();
 
         List<StoreMember> countQuery = jpaQueryFactory
                 .selectFrom(storeMember)
@@ -76,39 +72,7 @@ public class StoreListRepositoryImpl implements StoreListRepository {
 
     @Override
     public Slice<StoreDto> getFoodList(Pageable pageable) {
-        List<StoreDto> shopList = jpaQueryFactory
-                .select(Projections.constructor(StoreDto.class,
-                        storeMember.id,
-                        storeMember.storeName,
-                        storeMember.storeInfo.storePhoneNumber,
-                        storeMember.storeInfo.openTime,
-                        storeMember.storeInfo.closeTime,
-                        ExpressionUtils.as(
-                                JPAExpressions
-                                        .select(storeImage.thumbnailName)
-                                        .from(storeImage, storeImage)
-                                        .where(storeImage.id.eq(
-                                                JPAExpressions
-                                                        .select(storeImage.id.min())
-                                                        .from(storeImage, storeImage)
-                                                        .where(storeImage.storeMember.id.eq(storeMember.id)))),"listThumbnail"),
-                        ExpressionUtils.as(
-                                JPAExpressions
-                                        .select(storeMenu.menuName)
-                                        .from(storeMenu, storeMenu)
-                                        .where(storeMenu.id.eq(
-                                                JPAExpressions
-                                                        .select(storeMenu.id.min())
-                                                        .from(storeMenu, storeMenu)
-                                                        .where(storeMenu.storeMember.id.eq(storeMember.id)))),"menuName"),
-                        ExpressionUtils.as(
-                                JPAExpressions
-                                        .select(likes.id.count())
-                                        .from(likes, likes)
-                                        .where(likes.storeMember.id.eq(storeMember.id)),"likes")
-
-                ))
-                .from(storeMember, storeMember)
+        List<StoreDto> foodList = foodProjections()
                 .where(storeMember._super.userRole.eq(STORE),
                         storeMember.storeType.ne(StoreType.ECO_SHOP))
                 .offset(pageable.getOffset())
@@ -119,12 +83,52 @@ public class StoreListRepositoryImpl implements StoreListRepository {
                 .selectFrom(storeMember)
                 .fetch();
 
-        return new PageImpl<>(shopList, pageable, countQuery.size());
+        return new PageImpl<>(foodList, pageable, countQuery.size());
     }
 
     @Override
+    public Slice<StoreDto> getFoodList(Pageable pageable, SearchCondition searchCondition) {
+        List<StoreDto> foodList = foodProjections()
+                .where(storeMember._super.userRole.eq(STORE),
+                        storeMember.storeType.ne(StoreType.ECO_SHOP),
+                        isSearch(searchCondition.getStoreSearchType(), searchCondition.getContent()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        List<StoreMember> countQuery = jpaQueryFactory
+                .selectFrom(storeMember)
+                .where(storeMember._super.userRole.eq(STORE),
+                        storeMember.storeType.ne(StoreType.ECO_SHOP),
+                        isSearch(searchCondition.getStoreSearchType(), searchCondition.getContent()))
+                .fetch();
+
+        return new PageImpl<>(foodList, pageable, countQuery.size());
+    }
+
+
+    @Override
     public Slice<StoreDto> getFoodTypeList(Pageable pageable, StoreType storeType) {
-        List<StoreDto> shopList = jpaQueryFactory
+        List<StoreDto> foodTypeList = foodProjections()
+                .where(storeMember._super.userRole.eq(STORE),
+                        storeMember.storeType.ne(StoreType.ECO_SHOP),
+                        storeMember.storeType.eq(storeType))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        List<StoreMember> countQuery = jpaQueryFactory
+                .selectFrom(storeMember)
+                .where(storeMember._super.userRole.eq(STORE),
+                        storeMember.storeType.ne(StoreType.ECO_SHOP),
+                        storeMember.storeType.eq(storeType))
+                .fetch();
+
+        return new PageImpl<>(foodTypeList, pageable, countQuery.size());
+    }
+
+    private JPAQuery<StoreDto> shopProjections() {
+        return jpaQueryFactory
                 .select(Projections.constructor(StoreDto.class,
                         storeMember.id,
                         storeMember.storeName,
@@ -139,7 +143,7 @@ public class StoreListRepositoryImpl implements StoreListRepository {
                                                 JPAExpressions
                                                         .select(storeImage.id.min())
                                                         .from(storeImage, storeImage)
-                                                        .where(storeImage.storeMember.id.eq(storeMember.id)))),"listThumbnail"),
+                                                        .where(storeImage.storeMember.id.eq(storeMember.id)))), "listThumbnail"),
                         ExpressionUtils.as(
                                 JPAExpressions
                                         .select(storeMenu.menuName)
@@ -148,26 +152,80 @@ public class StoreListRepositoryImpl implements StoreListRepository {
                                                 JPAExpressions
                                                         .select(storeMenu.id.min())
                                                         .from(storeMenu, storeMenu)
-                                                        .where(storeMenu.storeMember.id.eq(storeMember.id)))),"menuName"),
+                                                        .where(storeMenu.storeMember.id.eq(storeMember.id)))), "menuName"),
                         ExpressionUtils.as(
                                 JPAExpressions
                                         .select(likes.id.count())
                                         .from(likes, likes)
-                                        .where(likes.storeMember.id.eq(storeMember.id)),"likes")
+                                        .where(likes.storeMember.id.eq(storeMember.id)), "likes")
                 ))
-                .from(storeMember, storeMember)
-                .innerJoin(storeMember).on(storeMember.id.eq(storeImage.storeMember.id))
-                .where(storeMember._super.userRole.eq(STORE),
-                        storeMember.storeType.ne(StoreType.ECO_SHOP),
-                        storeMember.storeType.eq(storeType))
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetch();
-
-        List<StoreMember> countQuery = jpaQueryFactory
-                .selectFrom(storeMember)
-                .fetch();
-
-        return new PageImpl<>(shopList, pageable, countQuery.size());
+                .from(storeMember, storeMember);
     }
+
+    private JPAQuery<StoreDto> foodProjections() {
+        return jpaQueryFactory
+                .select(Projections.constructor(StoreDto.class,
+                        storeMember.id,
+                        storeMember.storeName,
+                        storeMember.storeInfo.storePhoneNumber,
+                        storeMember.storeInfo.openTime,
+                        storeMember.storeInfo.closeTime,
+                        ExpressionUtils.as(
+                                JPAExpressions
+                                        .select(storeImage.thumbnailName)
+                                        .from(storeImage, storeImage)
+                                        .where(storeImage.id.eq(
+                                                JPAExpressions
+                                                        .select(storeImage.id.min())
+                                                        .from(storeImage, storeImage)
+                                                        .where(storeImage.storeMember.id.eq(storeMember.id)))), "listThumbnail"),
+                        ExpressionUtils.as(
+                                JPAExpressions
+                                        .select(storeMenu.menuName)
+                                        .from(storeMenu, storeMenu)
+                                        .where(storeMenu.id.eq(
+                                                JPAExpressions
+                                                        .select(storeMenu.id.min())
+                                                        .from(storeMenu, storeMenu)
+                                                        .where(storeMenu.storeMember.id.eq(storeMember.id)))), "menuName"),
+                        ExpressionUtils.as(
+                                JPAExpressions
+                                        .select(likes.id.count())
+                                        .from(likes, likes)
+                                        .where(likes.storeMember.id.eq(storeMember.id)), "likes")
+                ))
+                .from(storeMember, storeMember);
+    }
+
+//    private Expression<Long> likesCount() {
+//        return ExpressionUtils.as(
+//                JPAExpressions
+//                        .select(likes.id.count())
+//                        .from(likes, likes)
+//                        .where(likes.storeMember.id.eq(storeMember.id)), "likes");
+//
+//    }
+
+    /*
+     * 검색 조건
+     * */
+    private BooleanExpression eqStoreName(String storeName) {
+        return StringUtils.hasText(storeName) ? storeMember.storeName.containsIgnoreCase(storeName) : null;
+    }
+
+    private BooleanExpression eqMenuName(String menuName) {
+        return StringUtils.hasText(menuName) ? storeMenu.menuName.containsIgnoreCase(menuName) : null;
+    }
+
+    private BooleanExpression isSearch(StoreSearchType searchType, String searchText) {
+        if (searchType.equals(StoreSearchType.store_name)) {
+
+            return eqStoreName(searchText);
+        } else if (searchType.equals(StoreSearchType.item)) {
+            return eqMenuName(searchText);
+        } else {
+            return eqMenuName(searchText).or(eqStoreName(searchText));
+        }
+    }
+
 }
